2f1c6c53cac20959e288983580cad0da18165c8d
이채영 <160698032+alwaysY0ung@users.noreply.github.com>
2024-07-25 23:17:39 +0900

    our springboot project just started

---
+++ src/test/java/com/smuTree/Back_main/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smuTree.Back_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
